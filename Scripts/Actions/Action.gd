088dc7cfb1f8eb030f02f3984135513cbf5369ad
EveningComet <moist@debian>
2024-04-16 14:19:21 -0400

    Imported Pennycook's Utility AI into the project.

--- Scripts/Actions/Action.gd
+++ Scripts/Actions/Action.gd
@@ -3,3 +3,15 @@ class_name Action
 
 ## The character that the action is for.
 var actioner
+
+var action_type: Action.ActionTypes
+
+enum ActionTypes {
+	FollowPlayer,
+	
+	## Action for when the AI has nothing else to do. For example, an ally is
+	## within a few tiles away from the player.
+	Wait,
+	
+	FightEnemy
+}
--- Scripts/Components/AI/FriendlyBrain.gd
+++ Scripts/Components/AI/FriendlyBrain.gd
@@ -4,6 +4,9 @@ class_name FriendlyBrain extends AIBrain
 var player: Pawn
 var player_currently_controlling: bool = false
 
+@onready var follow_player_behavior: UtilityAIBehavior = preload("res://Game Data/Behaviors/Follow Player.tres")
+@onready var friendly_wait: UtilityAIBehavior = preload("res://Game Data/Behaviors/Friendly Wait.tres")
+
 func set_player(new_player: Pawn) -> void:
 	player = new_player
 	
@@ -18,30 +21,58 @@ func operate() -> void:
 		get_parent().get_node("Pawn").finished_turn.emit( null )
 		return
 
-	path = pathfinder.astar.get_id_path(
-		pathfinder.tile_map.local_to_map( get_parent().global_position ), 
-		pathfinder.tile_map.local_to_map( player.get_parent().global_position )
-	).slice(1)
+	# Time to decide what to do
+	var choices: Array[UtilityAIOption] = []
 	
-	if path.size() == 1:
-		get_parent().get_node("Pawn").finished_turn.emit(null)
-		return
+	var potential_action: Action = Action.new()
+	potential_action.action_type = Action.ActionTypes.FollowPlayer
 	
-	if path.is_empty() == true:
-		get_parent().get_node("Pawn").finished_turn.emit(null)
-		return
+	# Used by the AI to make a decision
+	var context: Dictionary = {
+		"distance_from_player": get_parent().global_position.distance_to( player.get_parent().global_position),
+		"my_hp": get_parent().get_node("Stats").stats[StatTypes.stat_types.CurrentHP],
+		"num_enemies_nearby": 0
+	}
 	
-	var dir = (path[0] - pathfinder.tile_map.local_to_map(get_parent().global_position))
-	get_parent().get_node("Pawn").update_raycast(dir)
-	if get_parent().get_node("Pawn").ray.is_colliding() == true:
-		get_parent().get_node("Pawn").finished_turn.emit(null)
-		return
+	var choice = UtilityAIOption.new(
+		follow_player_behavior, context, potential_action
+	)
+	choices.append( choice )
 	
-	# Checking to make sure the tile is valid
-	#var tile_data = pathfinder.tile_map.get_cell_tile_data(0, path[0])
-	#if tile_data != null and tile_data.get_custom_data("Type") == "Impassable":
-		#get_parent().get_node("Pawn").finished_turn.emit(null)
-		#return
+	potential_action = Action.new()
+	potential_action.action_type = Action.ActionTypes.Wait
+	choice = UtilityAIOption.new(
+		friendly_wait, context, potential_action
+	)
+	choices.append(choice)
 	
-	var p = pathfinder.tile_map.map_to_local(path[0])
-	get_parent().get_node("Mover").move( p )
+	var decision = UtilityAI.choose_highest( choices, 0.9 )
+	
+	var chosen_action: Action = decision.action as Action
+	match chosen_action.action_type:
+		Action.ActionTypes.FollowPlayer:
+			path = pathfinder.astar.get_id_path(
+				pathfinder.tile_map.local_to_map( get_parent().global_position ), 
+				pathfinder.tile_map.local_to_map( player.get_parent().global_position )
+			).slice(1)
+			
+			if path.size() == 1:
+				get_parent().get_node("Pawn").finished_turn.emit(chosen_action)
+				return
+			
+			if path.is_empty() == true:
+				get_parent().get_node("Pawn").finished_turn.emit(chosen_action)
+				return
+			
+			var dir = (path[0] - pathfinder.tile_map.local_to_map(get_parent().global_position))
+			get_parent().get_node("Pawn").update_raycast(dir)
+			if get_parent().get_node("Pawn").ray.is_colliding() == true:
+				get_parent().get_node("Pawn").finished_turn.emit(chosen_action)
+				return
+			
+			var p = pathfinder.tile_map.map_to_local(path[0])
+			get_parent().get_node("Mover").move( p )
+		
+		Action.ActionTypes.Wait:
+			get_parent().get_node("Pawn").finished_turn.emit(chosen_action)
+			return
